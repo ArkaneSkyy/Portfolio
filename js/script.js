81f3a292953d867149cc1aa926c7595a89e7e03a
ArkaneSkyy <74531315+ArkaneSkyy@users.noreply.github.com>
2021-12-02 21:44:44 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const homeMenu = document.querySelector('#home-page');
-if (window.innerWidth > 960) {
+if (window.innerWidth > 1200) {
 homeMenu.classList.add('highlight');
 }
 
@@ -10,27 +10,27 @@ window.addEventListener('scroll', () => {
     const contactMenu = document.querySelector('#contact-page');
     const scrollPos = window.scrollY;
     console.log(scrollPos);
-    if (window.innerWidth > 960 && scrollPos < 600) {
+    if (window.innerWidth > 1200 && scrollPos < 600) {
         homeMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         return;
-      } else if (window.innerWidth > 960 && scrollPos < 1300) {
+      } else if (window.innerWidth > 1200 && scrollPos < 1300) {
         aboutMenu.classList.add('highlight');
         homeMenu.classList.remove('highlight');
         skillsMenu.classList.remove('highlight');
         return;
-      } else if (window.innerWidth > 960 && scrollPos < 2500) {
+      } else if (window.innerWidth > 1200 && scrollPos < 2500) {
         skillsMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         projectMenu.classList.remove('highlight');
         homeMenu.classList.remove('highlight');
         return;
-      } else if (window.innerWidth > 960 && scrollPos < 2900) {
+      } else if (window.innerWidth > 1200 && scrollPos < 2900) {
         projectMenu.classList.add('highlight')
         skillsMenu.classList.remove('highlight');
         contactMenu.classList.remove('highlight');
         homeMenu.classList.remove('highlight');
-      } else if (window.innerWidth > 960 && scrollPos < 4000) {
+      } else if (window.innerWidth > 1200 && scrollPos < 4000) {
         contactMenu.classList.add('highlight');
         projectMenu.classList.remove('highlight');
         homeMenu.classList.remove('highlight');
