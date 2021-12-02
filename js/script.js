707fe214cc3b48b40cd95a227c92aad499072f30
ArkaneSkyy <74531315+ArkaneSkyy@users.noreply.github.com>
2021-12-02 21:36:57 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
+const homeMenu = document.querySelector('#home-page');
+if (window.innerWidth > 960) {
+homeMenu.classList.add('highlight');
+}
+
 window.addEventListener('scroll', () => {
-    const elem = document.querySelector('.highlight');
-    const homeMenu = document.querySelector('#home-page');
     const aboutMenu = document.querySelector('#about-page');
     const skillsMenu = document.querySelector('#skills-page');
     const projectMenu = document.querySelector('#projects-page');
     const contactMenu = document.querySelector('#contact-page');
     const scrollPos = window.scrollY;
-   // console.log(scrollPos);
+    console.log(scrollPos);
     if (window.innerWidth > 960 && scrollPos < 600) {
         homeMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         return;
-      } else if (window.innerWidth > 960 && scrollPos < 1500) {
+      } else if (window.innerWidth > 960 && scrollPos < 1300) {
         aboutMenu.classList.add('highlight');
         homeMenu.classList.remove('highlight');
         skillsMenu.classList.remove('highlight');
@@ -19,16 +22,20 @@ window.addEventListener('scroll', () => {
       } else if (window.innerWidth > 960 && scrollPos < 2500) {
         skillsMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
-        projectMenu.classList.remove('highlight')
+        projectMenu.classList.remove('highlight');
+        homeMenu.classList.remove('highlight');
         return;
-      } else if (window.innerWidth > 960 && scrollPos < 3000) {
+      } else if (window.innerWidth > 960 && scrollPos < 2900) {
         projectMenu.classList.add('highlight')
         skillsMenu.classList.remove('highlight');
         contactMenu.classList.remove('highlight');
+        homeMenu.classList.remove('highlight');
       } else if (window.innerWidth > 960 && scrollPos < 4000) {
         contactMenu.classList.add('highlight');
-        projectMenu.classList.remove('highlight')
-      }
-      
+        projectMenu.classList.remove('highlight');
+        homeMenu.classList.remove('highlight');
+      } 
 })
+
+    
    
